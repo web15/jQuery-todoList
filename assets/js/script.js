69fc237f2cb48dea93df93175804eef53025b401
Charles Marks <clmarks15@gmail.com>
2019-01-10 12:44:55 -0500

    ready4styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,23 @@
 // Check off specific Todos by clicking
-$("li").click(function () {
+$("ul").on("click", "li", function () {
         $(this).toggleClass("completed");
+});
+
+// Click on X to delete Todo
+$("ul").on("click", "span", function(e) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+    e.stopPropagation();
+});
+
+// Obtain text input and use
+$("input[type='text']").keypress(function(e) {
+        if(e.which === 13) {
+            // grabbing new todo text from input
+            var todoText = $(this).val();
+            $(this).val("");
+            // create a new li and add to ul
+            $("ul").append("<li><span>X</span> " + todoText + "</li>")
+        }
 });
